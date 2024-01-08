331c0422680305471fbd2793d6f5f0f420d72626
nizarkrimis <nizar_id@live.fr>
2024-01-08 10:55:12 +0100

    create micro service

---
+++ cour/src/test/java/com/example/cour/CourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
